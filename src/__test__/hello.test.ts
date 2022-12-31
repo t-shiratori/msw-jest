bf73526f2611c583824451cc007246d878c38ca6
Takashi Shiratori <tks.srtr89@gmail.com>
2022-12-31 14:49:56 +0900

    delete files

--- src/__test__/hello.test.ts
+++
@@ -1,5 +0,0 @@
-import { sumNumbers } from '../sumNumbers';
-
-test('1 + 2 = 3', () => {
-  expect(sumNumbers(1, 2)).toBe(3);
-});
--- src/sumNumbers.ts
+++
@@ -1,3 +0,0 @@
-export const sumNumbers = (a: number, b: number): number => {
-  return a + b;
-};
