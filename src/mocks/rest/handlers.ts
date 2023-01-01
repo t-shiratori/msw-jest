7ded2dcc66c8aa9cf3f4f11c66b21d2214376ad0
Takashi Shiratori <tks.srtr89@gmail.com>
2023-01-02 01:11:50 +0900

    delete code

--- src/mocks/rest/handlers.ts
+++ src/mocks/rest/handlers.ts
@@ -2,9 +2,6 @@ import { rest } from 'msw';
 import { ORIGIN } from '../../const';
 
 export const handlers = [
-  rest.post(`${ORIGIN}/login`, (req, res, ctx) => {
-    return res(ctx.status(200));
-  }),
   rest.get(`${ORIGIN}/user`, (req, res, ctx) => {
     return res(
       ctx.status(200),
