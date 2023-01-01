dc1171846b819402407217d84e9e0ec884c85f23
Takashi Shiratori <tks.srtr89@gmail.com>
2023-01-01 23:13:37 +0900

    delete comments

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,7 +1,5 @@
-// src/mocks/server.js
 import { setupServer } from 'msw/node';
 import { handlers as resetHandlers } from './rest/handlers';
 import { handlers as graphqlHandler } from './graphql/handlers';
 
-// This configures a request mocking server with the given request handlers.
 export const server = setupServer(...resetHandlers, ...graphqlHandler);
