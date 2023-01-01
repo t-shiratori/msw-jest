19137f549f2dabd746351964badeeee1ea86ccb4
Takashi Shiratori <tks.srtr89@gmail.com>
2023-01-01 20:06:57 +0900

    delete comment

--- src/mocks/graphql/handlers.ts
+++ src/mocks/graphql/handlers.ts
@@ -1,10 +1,7 @@
-// src/mocks/handlers.js
 import { graphql } from 'msw';
 
 export const handlers = [
-  // Handles a "GetUserInfo" query
   graphql.query('GetUserInfo', (req, res, ctx) => {
-    // When authenticated, respond with a query payload
     return res(
       ctx.data({
         user: {
